768f8a87860693f07da5b7bb8e0b0dd8818ccac9
Sheila Nyambura <sheilanyambura394@gmail.com>
2023-12-11 12:08:58 +0300

    created a function and tested it

--- consonant.py
+++ consonant.py
@@ -1,2 +1,20 @@
-#print("Hello world")
+def solve(word):
+   
+    vowels = set("aeiou")
+    substrings = []
+    start = 0
+
+    for i in range(len(word)):
+        if word[i] in vowels:
+            substrings.append(word[start:i])
+            start = i + 1
+    substrings.append(word[start:]) 
+    substrings = [sum(ord(c) - ord('a') + 1 for c in substring) for substring in substrings]
+
+    return max(substrings)
+
+print(solve("strength")) 
+print(solve("zodiac")) 
+print(solve("phase")) 
+
 
