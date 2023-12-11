7ccb3e1592e227f37815997ac407df2867dd76ad
Sheila Nyambura <sheilanyambura394@gmail.com>
2023-12-11 11:46:13 +0300

    testing the correctnessof the function

--- number.py
+++ number.py
@@ -11,5 +11,8 @@ def two_positive(a, b, c):
     return positive_count == 2
 
 
- 
- 
+print(two_positive(-12, 4, -3))  # Output False
+print(two_positive(4, 5, 8))     # Output False
+print(two_positive(6, -8, 9))    # Output True
+print(two_positive(6, 18, 9)) 
+print(two_positive(6, -468, 9))
